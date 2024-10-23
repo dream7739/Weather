58447d12dd295e686bc99f3736475f58f9c86c1a
홍정민 <dream7739@naver.com>
2024-10-24 00:25:15 +0900

    SearchViewController 추가 및 디자인 반영

---
+++ Weather/Scene/Weather/CitySearchViewController.swift
@@ -0,0 +1,15 @@
+//
+//  CitySearchViewController.swift
+//  Weather
+//
+//  Created by 홍정민 on 10/23/24.
+//
+
+import Foundation
+
+final class CitySearchViewController: BaseViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print(#function)
+    }
+}
--- Weather/Scene/Weather/WeatherViewController.swift
+++ Weather/Scene/Weather/WeatherViewController.swift
@@ -8,19 +8,30 @@
 import UIKit
 import RxSwift
 import RxCocoa
+import SnapKit
 
 final class WeatherViewController: BaseViewController {
-
+    private let cityViewController = CitySearchViewController()
+    private lazy var searchController = UISearchController(searchResultsController: cityViewController)
     private let viewModel = WeatherViewModel()
     private let disposeBag = DisposeBag()
+    
+    enum Section: Int, CaseIterable {
+         case main
+         case hourly
+         case weekly
+         case map
+         case details
+     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         bind()
+        configureSearchController()
     }
-
+    
     func bind() {
-        var input = WeatherViewModel.Input(
+        let input = WeatherViewModel.Input(
             callWeatherRequest: BehaviorRelay(
                 value: Coord(
                     lat: 36.783611,
@@ -29,8 +40,24 @@ final class WeatherViewController: BaseViewController {
             )
         )
         
-        var output = viewModel.transform(input: input)
+        let output = viewModel.transform(input: input)
     }
-
+    
 }
 
+extension WeatherViewController {
+    private func configureSearchController() {
+         searchController.searchBar.placeholder = "Search"
+         navigationItem.searchController = searchController
+         searchController.searchBar.searchBarStyle = .minimal
+         searchController.searchBar.backgroundColor = .clear
+         searchController.searchBar.searchTextField.backgroundColor = .systemGray6.withAlphaComponent(0.6)
+         searchController.searchResultsUpdater = self
+     }
+}
+
+extension WeatherViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        
+    }
+}
