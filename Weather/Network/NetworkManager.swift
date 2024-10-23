45c99da1c5bf0afe205645c213b4394b31d1a939
홍정민 <dream7739@naver.com>
2024-10-23 19:58:06 +0900

    NetworkManager 생성 및 네트워크 통신 코드 작성

---
+++ Weather/Network/NetworkManager.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkManager.swift
+//  Weather
+//
+//  Created by 홍정민 on 10/23/24.
+//
+
+import Foundation
+import Alamofire
+import RxSwift
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    func callRequest(_ coord: Coord) -> Single<Result<WeatherResult, Error>> {
+        let url = APIURL.forecast + "?lat=\(coord.lat)&lon=\(coord.lon)&appid=\(APIKey.weather)"
+        print(#function)
+        let result = Single<Result<WeatherResult, Error>>.create { observer in
+            AF.request(url)
+                .validate(statusCode: 200...400)
+                .responseDecodable(of: WeatherResult.self) { response in
+                    switch response.result {
+                    case .success(let value):
+                        observer(.success(.success(value)))
+                    case .failure(let error):
+                        observer(.success(.failure(error)))
+                    }
+                }
+            return Disposables.create()
+        }
+        
+        return result
+    }
+}
--- Weather/Scene/Weather/WeatherViewController.swift
+++ Weather/Scene/Weather/WeatherViewController.swift
@@ -6,9 +6,13 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 final class WeatherViewController: UIViewController {
 
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
