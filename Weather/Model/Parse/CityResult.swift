c2855a78401b0b1ac31d93bc5ad525677532b059
홍정민 <dream7739@naver.com>
2024-10-25 16:35:15 +0900

    파싱 모델 별도 생성 및 CitySearchViewModel 파싱 로직 구성

---
+++ Weather/Model/Parse/CityResult.swift
@@ -0,0 +1,8 @@
+//
+//  CityResult.swift
+//  Weather
+//
+//  Created by 홍정민 on 10/25/24.
+//
+
+import Foundation
---
+++ Weather/Scene/Search/CitySearchViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  CitySearchViewModel.swift
+//  Weather
+//
+//  Created by 홍정민 on 10/25/24.
+//
+
+import Foundation
